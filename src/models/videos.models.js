7ca28fb8501613e2e3c650a5755f890d3e6e0ebb
Jose-Tejero <j_jtejero@hotmail.com>
2022-11-12 21:55:18 -0600

    Modelos de tablas principales

--- src/models/videos.models.js
+++ src/models/videos.models.js
@@ -1,20 +1,29 @@
 const db = require('../utils/database');
 const { DataTypes } = require('sequelize');
 
-const Categories = db.define('categories', {
+const Videos = db.define('videos', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
     allowNull: false,
   },
-  name: {
+  title: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
   },
-},{
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  course_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+}, {
   timestamps: false,
 });
 
-module.exports = Categories;
+module.exports = Videos;
