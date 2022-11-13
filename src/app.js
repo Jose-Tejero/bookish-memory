b465405a626588e6e484ef5a82e836a919f8cb42
Jose-Tejero <j_jtejero@hotmail.com>
2022-11-12 18:54:01 -0600

    Corriendo servidor

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('Hola mundo');
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+})
