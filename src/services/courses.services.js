4cf882c557725c0be7525beb045e81b10b637b52
Jose-Tejero <j_jtejero@hotmail.com>
2022-11-14 00:05:12 -0600

    Servicio creado para mostrar videos y categorías en cursos

--- src/services/courses.services.js
+++ src/services/courses.services.js
@@ -1,6 +1,7 @@
 const Categories = require("../models/categories.models");
 const Courses = require("../models/courses.models");
 const CoursesCategories = require("../models/coursesCategories.models");
+const Videos = require("../models/videos.models");
 
 class CoursesServices {
   static async getAll() {
@@ -16,14 +17,20 @@ class CoursesServices {
     try {
       const result = await Courses.findAll({
         attributes: ['title'],
-        include: {
+        include: [{
           model: CoursesCategories,
           as: 'categories',
+          attributes: ['category_id'],
           include: {
             model: Categories,
             as: 'category',
+            attributes: ['name'],
           },
-        },
+        }, {
+          model: Videos,
+          as: 'video',
+          attributes: ['title', 'url'],
+        }],
       });
       return result;
     } catch (error) {
